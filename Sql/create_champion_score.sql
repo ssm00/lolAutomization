b5691bba54dc5d535c6d62cf24e7acefd38454a1
ssm00 <titan0724@mju.ac.kr>
2024-12-30 20:51:06 +0900

    feat:sql

---
+++ Sql/create_champion_score.sql
@@ -0,0 +1,26 @@
+create table champion_score_support(
+	seq int auto_increment primary key,
+    name_us varchar(50),
+    name_kr varchar(50),
+    champion_tier int,
+    ranking int,
+    ranking_variation int,
+    is_op boolean,
+    ps_score decimal(4,2),
+    honey_score decimal(4,2),
+    win_rate decimal(4,2),
+    pick_rate decimal(4,2),
+    ban_rate decimal(4,2),
+    sample_size int,
+    patch decimal(4,2),
+    survey_target_tier varchar(20),
+    region varchar(20),
+    updated_at datetime,
+    unique key unique_patch_champ (patch, name_us)
+);
+
+drop table champion_score_top;
+select * from champion_score_top;
+
+
+
