c6c6e599830178095b1ee6dbe4fd6ad8bd408d9a
ssm00 <titan0724@mju.ac.kr>
2025-01-24 17:17:22 +0900

    Update metadata.py

--- MyMetaData/metadata.py
+++ MyMetaData/metadata.py
@@ -1,14 +1,15 @@
 import json
-import sys
-import os
 from pathlib import Path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class MetaData:
 
-    def __init__(self, config_dir='./MyMetaData'):
+    def __init__(self):
         self.db_info = None
-        self.config_dir = Path(config_dir)
+        self.anomaly_info = None
+        self.prompt = None
+        self.key = None
+        self.image_modifier_info = None
+        self.config_dir = Path(__file__).parent
         self.load_all_json()
 
     def load_json(self, file_name):
@@ -18,4 +19,8 @@ class MetaData:
 
     def load_all_json(self):
         self.db_info = self.load_json('db_info.json')
+        self.anomaly_info = self.load_json('anomaly_detection_info.json')
+        self.prompt = self.load_json('prompt.json')
+        self.key = self.load_json('key.json')
+        self.image_modifier_info = self.load_json('image_modifier_info.json')
 
