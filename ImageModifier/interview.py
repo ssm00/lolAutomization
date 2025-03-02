e347d93cdd0be036845c2253cbc597a46152f020
ssm00 <titan0724@mju.ac.kr>
2025-03-03 02:21:50 +0900

    Update interview.py

--- ImageModifier/interview.py
+++ ImageModifier/interview.py
@@ -16,28 +16,18 @@ import pandas as pd
 class Interview:
 
     def __init__(self, database, meta_data):
+        super().__init__(database, meta_data)
         self.database = database
         self.meta_data = meta_data
         self.properties = meta_data.image_modifier_info
-        self.champion_background_dir = Path(__file__).parent.parent / "Assets" / "Image" / "champion"
-        self.champion_icon_dir = Path(__file__).parent.parent / "Assets" / "Image" / "champion_icon"
-        self.player_dir = Path(__file__).parent.parent / "Assets" / "Image" / "player"
-        self.team_icon_dir = Path(__file__).parent.parent / "Assets" / "Image" / "team_icon"
-        self.background_dir = Path(__file__).parent.parent / "Assets" / "Image" / "background"
-        self.pick_rate_assets_dir = Path(__file__).parent.parent / "Assets" / "PickRate"
-        self.plt_dir = Path(__file__).parent.parent / "PltOutput"
-        self.output_dir = Path(__file__).parent.parent / "ImageOutput" / "PickRate"
-        self.title_font_path = Path(__file__).parent.parent / "Assets" / "Font" / "Cafe24Ohsquare-v2.0" / "Cafe24Ohsquare-v2.0.ttf"
-        self.main_font_path = Path(__file__).parent.parent / "Assets" / "Font" / "Noto_Sans_KR" / "static" / "NotoSansKR-Bold.ttf"
-        self.anton_font_path = Path(__file__).parent.parent / "Assets" / "Font" / "Anton,Noto_Sans_KR" / "Anton" / "Anton-Regular.ttf"
-        self.noto_font_path = Path(__file__).parent.parent / "Assets" / "Font" / "Noto_Sans_KR" / "NotoSansKR-VariableFont_wght.ttf"
-        self.main_font_size = self.properties.get("main_font_size")
-        self.main_line_spacing = self.properties.get("main_line_spacing")
+
+        #path
+        self.title_background_dir = Path(__file__).parent.parent / "Assets" / "Interview" / "title.png"
+        self.main_background_dir = Path(__file__).parent.parent / "Assets" / "Interview" / "main.png"
+        self.output_dir = Path(__file__).parent.parent / "ImageOutput" / "Interview"
+
+        # properties
         self.title_font_size = self.properties.get("title_font_size")
-        self.black = self.properties.get("black")
-        self.tier_color = self.properties.get("tier_color")
-        self.plt_draw = PltDraw(database, meta_data)
-        self.article_generator = ArticleGenerator(database, meta_data)
 
     def title_page(self):
         pass
