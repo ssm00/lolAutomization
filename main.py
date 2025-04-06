9cde2f0110bbf8521c551c3f1f5ada2c1190c6bb
ssm00 <titan0724@mju.ac.kr>
2025-04-07 00:06:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import datetime
+import traceback
+
 from Db.mysql_db import Database
 from MyMetaData.metadata import MetaData
 from DataSource.oracle_elixir.oracle_elixir_downloader import OracleElixirDownloader
@@ -10,42 +13,93 @@ import logging
 from Db.mongo_db import MongoDB
 from ImageModifier.match_result import MatchResult
 from ImageModifier.interview import Interview
-
+from DataSource.youtube.lck_official import LCKOfficial
 
 
 class Main:
 
     def __init__(self, database, mongo, meta_data, logger):
-        self.database = database,
-        self.mongo = mongo,
-        self.meta_data = meta_data,
-        self.logger = logger,
+        self.database = database
+        self.mongo = mongo
+        self.meta_data = meta_data
+        self.logger = logger
         self.oracle_elixirs_downloader = OracleElixirDownloader(database)
         self.champion_score = ChampionScore(database)
         self.detection = ChampionDetection(database, meta_data)
         self.image_download = ImageDownload(database)
         self.article_generator = ArticleGenerator(database, mongo, meta_data)
-        self.match_result = MatchResult(database, meta_data)
+        self.match_result = MatchResult(database, meta_data, self.article_generator)
         self.interview = Interview(database, mongo, meta_data, self.article_generator)
         self.pick_rate = PickRate(database, meta_data, self.article_generator)
+        self.you_tube = LCKOfficial(mongo)
     
     # 1. oracle_elixirs 업데이트
     # 2. 패치 버전 업데이트 확인
     # 3. ps 챔피언 티어 업데이트
     # 4. 이상치 탐색 글 작성
-    # 5. slack 전송
+    # 5. 유튜브 영상 다운로드
+    # 6. 인터뷰 글 작성
+    # 7. slack 전송
     def daily_work(self):
         self.oracle_elixirs_downloader.update_oracle_elixirs()
+        self.database.update_patch_url_number()
+        self.detection.set_patch_version()
         self.champion_score.update_score()
-        pentakill_list = self.detection.run_penta_kill()
-        self.match_result.run(pentakill_list)
+        self.run_pick_rate()
+        self.run_match_result()
+        self.you_tube.download_videos_by_date()
+        self.interview.run()
+
+    # 리그 정보 업데이트
+    # 팀 정보 업데이트, 팀 아이콘 다운
+    def weekly_work(self):
+        self.image_download.run_league()
+        self.image_download.run_team()
+        self.database.match_team_name()
 
     def run_pick_rate(self):
-        self.pick_rate.fifth_page("LOLTMNT01_204281","GIDEON")
+        pick_rate_list = self.detection.run_pick_rate()
+        for pick_rate_info in pick_rate_list:
+            self.pick_rate.run(pick_rate_info['gameid'], pick_rate_info['player'])
 
+    # detection_type -> general, penta_kill, unmatch_line, two_bottom_choice
     def run_match_result(self):
-        self.match_result.run("LOLTMNT01_204281", "GIDEON", "type")
+        unmatch_line_list = self.detection.run_unmatch_line()
+        for unmatch_line in unmatch_line_list:
+            self.match_result.run(unmatch_line['gameid'], unmatch_line['playername'], "unmatch_line")
+        two_bottom_choice_list = self.detection.run_two_bottom_choice()
+        for two_bottom_choice in two_bottom_choice_list:
+            self.match_result.run(two_bottom_choice['gameid'], two_bottom_choice['playername'], "two_bottom_choice")
+        penta_kill_list = self.detection.run_penta_kill()
+        for penta_kill in penta_kill_list:
+            self.match_result.run(penta_kill['gameid'], penta_kill['playername'], "penta_kill")
 
+    def run_update_team_icon(self):
+        self.pick_rate.run("","","")
+
+    def run_interview(self):
+        self.interview.run()
+
+    def test_pick_rate(self):
+        pick_rate_list = self.detection.run_pick_rate("test")
+        for pick_rate in pick_rate_list:
+            print(pick_rate['gameid'])
+            self.pick_rate.run(pick_rate['gameid'], pick_rate['playername'])
+
+    def test_interview(self):
+        self.interview.test_run()
+
+    # match_result detection_type -> general, penta_kill, unmatch_line, two_bottom_choice
+    def test_match_result(self):
+        unmatch_line_list = self.detection.run_unmatch_line("test")
+        for unmatch_line in unmatch_line_list:
+            self.match_result.run(unmatch_line['gameid'], unmatch_line['playername'], "unmatch_line")
+        two_bottom_choice_list = self.detection.run_two_bottom_choice("test")
+        for two_bottom_choice in two_bottom_choice_list:
+            self.match_result.run(two_bottom_choice['gameid'], two_bottom_choice['playername'], "two_bottom_choice")
+        penta_kill_list = self.detection.run_penta_kill("test")
+        for penta_kill in penta_kill_list:
+            self.match_result.run(penta_kill['gameid'],penta_kill['playername'],"penta_kill")
 
 if __name__ == "__main__":
     logger = logging.getLogger("mylogger")
@@ -53,37 +107,4 @@ if __name__ == "__main__":
     database = Database(meta_data.db_info["mysql_local"], meta_data, logger=logger)
     mongo = MongoDB(meta_data.db_info["mongo_local"])
     main = Main(database, mongo, meta_data, logger)
-    #main.run_pick_rate()
-    main.run_match_result()
-    #main.daily_work()
-
-# detection.run_pick_rate()
-# detection.run_unmatch_line()
-# detection.run_two_bottom_choice()
-#detection.run_penta_kill()
-
-#detection.run_pick_rate()
-#detection.run_unmatch_line()
-#detection.run_performance_score()
-#detection.run_match_info()
-#detection.draw_all_series("LOLTMNT02_192520","Sylvie")
-#detection.draw_combined_series("LOLTMNT02_192520","Sylvie")
-#detection.get_game_mvp("LOLTMNT03_138902")
-
-
-#interview.summary_interview("../input.mp4")
-#interview.title_page("input.mp4")
-#interview.main_page("input.mp4")
-#match_result.title_page("LOLTMNT01_204281","GIDEON")
-#match_result.main_page("LOLTMNT01_204281","GIDEON")
-#pickrate = PickRate(database, meta_data, article_generator)
-#pickrate.first_page("LOLTMNT01_204281","GIDEON")
-#pickrate.second_page("LOLTMNT01_204281","GIDEON")
-#pickrate.third_page("LOLTMNT01_204281","GIDEON")
-#pickrate.fourth_page("LOLTMNT01_204281","GIDEON")
-#pickrate.fifth_page("LOLTMNT01_204281","GIDEON")
-#pickrate.run_all_page("LOLTMNT03_185067","Oner")
-#pickrate.create_champion_comparison()
-#pickrate.convert_png()
-# note = PatchNote(database, postgres)
-# note.update_patch_note()
+    main.test_pick_rate()
