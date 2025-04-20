5cfce76cc833c0e029208eb6e23e6d7eb8f2d119
ssm00 <titan0724@mju.ac.kr>
2025-04-20 23:02:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from Db.mysql_db import Database
 from MyMetaData.metadata import MetaData
 from DataSource.oracle_elixir.oracle_elixir_downloader import OracleElixirDownloader
 from DataSource.Lolps.champion_score import ChampionScore
+from DataSource.Lolps.champion_info_update import ChampionInfo
 from DataSource.LolOfficial.image_download import ImageDownload
 from AnomalyDetection.champion_detection import ChampionDetection
 from ImageModifier.pickrate import PickRate
@@ -14,7 +15,10 @@ from Db.mongo_db import MongoDB
 from ImageModifier.match_result import MatchResult
 from ImageModifier.interview import Interview
 from DataSource.youtube.lck_official import LCKOfficial
-
+from Aws.s3 import S3Manager
+from Slack.SlackClient import SlackClient
+from Instagram.instagram_uploader import InstagramUploader
+from MyMetaData.patch_version import Patch
 
 class Main:
 
@@ -23,16 +27,22 @@ class Main:
         self.mongo = mongo
         self.meta_data = meta_data
         self.logger = logger
+        self.patch = Patch(database, meta_data)
         self.oracle_elixirs_downloader = OracleElixirDownloader(database)
         self.champion_score = ChampionScore(database)
-        self.detection = ChampionDetection(database, meta_data)
+        self.detection = ChampionDetection(database, meta_data, self.patch)
         self.image_download = ImageDownload(database)
-        self.article_generator = ArticleGenerator(database, mongo, meta_data)
+        self.article_generator = ArticleGenerator(database, mongo, meta_data, self.patch)
         self.match_result = MatchResult(database, meta_data, self.article_generator)
         self.interview = Interview(database, mongo, meta_data, self.article_generator)
-        self.pick_rate = PickRate(database, meta_data, self.article_generator)
-        self.you_tube = LCKOfficial(mongo)
-    
+        self.pick_rate = PickRate(database, meta_data, self.article_generator, self.patch)
+        self.you_tube = LCKOfficial(meta_data, mongo)
+        self.s3_manager = S3Manager()
+        self.slack_client = SlackClient(database, meta_data.account_info, self.s3_manager, logger)
+        self.instagram_uploader = InstagramUploader(meta_data.account_info, logger)
+        self.champion_info_downloader = ChampionInfo(database)
+
+
     # 1. oracle_elixirs 업데이트
     # 2. 패치 버전 업데이트 확인
     # 3. ps 챔피언 티어 업데이트
@@ -41,53 +51,71 @@ class Main:
     # 6. 인터뷰 글 작성
     # 7. slack 전송
     def daily_work(self):
+        self.patch.set_patch_version()
         self.oracle_elixirs_downloader.update_oracle_elixirs()
         self.database.update_patch_url_number()
-        self.detection.set_patch_version()
         self.champion_score.update_score()
         self.run_pick_rate()
         self.run_match_result()
         self.you_tube.download_videos_by_date()
         self.interview.run()
+        self.s3_manager.upload_today_folders()
+        self.show_all_image()
 
     # 리그 정보 업데이트
     # 팀 정보 업데이트, 팀 아이콘 다운
+    # 선수는 너무 많아서 스킵
     def weekly_work(self):
         self.image_download.run_league()
         self.image_download.run_team()
         self.database.match_team_name()
+        self.champion_info_downloader.update_champion_info()
 
     def run_pick_rate(self):
         pick_rate_list = self.detection.run_pick_rate()
+        print(f"픽률 탐지 개수 : {len(pick_rate_list)}")
         for pick_rate_info in pick_rate_list:
-            self.pick_rate.run(pick_rate_info['gameid'], pick_rate_info['player'])
+            self.pick_rate.run(pick_rate_info['gameid'], pick_rate_info['playername'])
 
     # detection_type -> general, penta_kill, unmatch_line, two_bottom_choice
     def run_match_result(self):
         unmatch_line_list = self.detection.run_unmatch_line()
+        print(f"unmatch_line_list 탐지 개수 : {len(unmatch_line_list)}")
         for unmatch_line in unmatch_line_list:
             self.match_result.run(unmatch_line['gameid'], unmatch_line['playername'], "unmatch_line")
         two_bottom_choice_list = self.detection.run_two_bottom_choice()
+        print(f"two_bottom_choice_list 탐지 개수 : {len(two_bottom_choice_list)}")
         for two_bottom_choice in two_bottom_choice_list:
             self.match_result.run(two_bottom_choice['gameid'], two_bottom_choice['playername'], "two_bottom_choice")
         penta_kill_list = self.detection.run_penta_kill()
+        print(f"penta_kill_list 탐지 개수 : {len(penta_kill_list)}")
         for penta_kill in penta_kill_list:
             self.match_result.run(penta_kill['gameid'], penta_kill['playername'], "penta_kill")
 
-    def run_update_team_icon(self):
-        self.image_download.run_team()
+    def run_player_image(self):
+        self.image_download.run_player()
 
     def run_interview(self):
         self.interview.run()
 
+    def show_all_image(self):
+        try:
+            image_list = self.s3_manager.get_all_today_image()
+            self.slack_client.show_image_list(image_list)
+            self.logger.info("slack 이미지 전송 완료")
+        except Exception as e:
+            print(f"slack 이미지 전송 실패 메시지 {e}")
+
     def test_pick_rate(self):
         pick_rate_list = self.detection.run_pick_rate("test")
         for pick_rate in pick_rate_list:
-            print(pick_rate['gameid'])
             self.pick_rate.run(pick_rate['gameid'], pick_rate['playername'])
 
     def test_interview(self):
         self.interview.test_run()
+
+    def test_1(self):
+        self.champion_info_downloader.update_champion_info()
 
     # match_result detection_type -> general, penta_kill, unmatch_line, two_bottom_choice
     def test_match_result(self):
@@ -101,10 +129,11 @@ class Main:
         for penta_kill in penta_kill_list:
             self.match_result.run(penta_kill['gameid'],penta_kill['playername'],"penta_kill")
 
+
 if __name__ == "__main__":
     logger = logging.getLogger("mylogger")
     meta_data = MetaData()
-    database = Database(meta_data.db_info["mysql_local"], meta_data, logger=logger)
-    mongo = MongoDB(meta_data.db_info["mongo_local"])
+    database = Database(meta_data.db_info["mysql"], meta_data, logger=logger)
+    mongo = MongoDB(meta_data.db_info["mongo_atlas"])
     main = Main(database, mongo, meta_data, logger)
-    main.test_pick_rate()
+    main.daily_work()
